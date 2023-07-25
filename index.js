05134a476d5d948fcf2058d49a564eff4abbf68b
Misael Sena <misaellsena@gmail.com>
2023-07-25 20:34:42 -0300

    Conclusão dos Exercícios 1 e 2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const prompt = require('prompt-sync')();
+
+
+function exercio1(){
+    console.log("Hello World!");
+}
+// exercio1();
+
+function exercio2(){
+    let nome = prompt("Informe seu nome: ");
+    console.log(`Olá ${nome}!`);
+}
+
+exercio2();
