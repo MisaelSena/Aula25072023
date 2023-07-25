36a70129a4feb024a2e0d06aa85acbc12c406551
Misael Sena <misaellsena@gmail.com>
2023-07-25 20:41:58 -0300

    Conclusão do Exercício 3

--- index.js
+++ index.js
@@ -10,5 +10,16 @@ function exercio2(){
     let nome = prompt("Informe seu nome: ");
     console.log(`Olá ${nome}!`);
 }
+//exercio2();
 
-exercio2();
+function exercio3(){
+    let numero1 = parseInt(prompt("Informe o 1º Número Inteiro: "));
+    let numero2 = parseInt(prompt("Informe o 2º Número Inteiro: "));
+
+    let soma = numero1 + numero2;
+
+    console.log(`A soma dos números digitados foi: ${soma}`);
+
+}
+//exercio3();
+
