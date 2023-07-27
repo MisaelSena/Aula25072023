17554f12689088f4d3dd1a995978a677e1f6ee50
Misael Sena <misaellsena@gmail.com>
2023-07-26 22:19:16 -0300

    Conclusão do Exercício 6

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const prompt = require('prompt-sync')();
 
-
+//Escreva um programa que imprima "Olá, mundo!" na tela.
 function exercio1(){
     console.log("Hello World!");
 }
 //exercio1();
 
+//Crie um programa que solicite o nome do usuário e, em seguida, imprima uma mensagem personalizada com o nome inserido.
 function exercio2(){
     let nome = prompt("Informe seu nome: ");
     console.log(`Olá ${nome}!`);
 }
-
 //exercio2();
 
+//Escreva um programa que calcule a soma de dois números inteiros e exiba o resultado.
 function exercio3(){
     let numero1 = parseInt(prompt("Informe o 1º Número Inteiro: "));
     let numero2 = parseInt(prompt("Informe o 2º Número Inteiro: "));
@@ -24,6 +25,7 @@ function exercio3(){
 }
 //exercio3();
 
+//Crie um programa que receba a idade de uma pessoa e informe se ela é maior ou menor de idade.
 function exercio4(){
     let idade = parseInt(prompt("Informe a Idade: "));
 
@@ -42,6 +44,8 @@ function exercio4(){
     }
 }
 //exercio4();
+
+//Faça um programa que verifique se um número é par ou ímpar
 function exercio5(){
     let numero = parseInt(prompt("Informe um número inteiro: "));
 
@@ -56,4 +60,27 @@ function exercio5(){
         return exercio5();
     }
 }
-exercio5();
+//exercio5();
+
+//Escreva um programa que calcule a média de três números informados pelo usuário
+function exercio6(){
+    let numeros = [];
+    
+
+    while(numeros.length < 3){
+        let numero = parseFloat(prompt("Informe um número inteiro: "));       
+
+        if(isNaN(numero)){
+            console.log("Informe um número válido!");
+        }else{
+            numeros.push(numero);
+            console.log(numeros);
+        }
+
+        let media = (numeros[0]+numeros[1]+numeros[2])/3;
+
+        console.log(`A média dos números informados é: ${media}`);
+    }
+    
+}
+exercio6();
