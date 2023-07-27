ea0fa78c7ff9b4d35d14771905c31496632d921b
Misael Sena <misaellsena@gmail.com>
2023-07-26 21:11:21 -0300

    Conclusão do Exercício 4

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const prompt = require('prompt-sync')();
 function exercio1(){
     console.log("Hello World!");
 }
-// exercio1();
+//exercio1();
 
 function exercio2(){
     let nome = prompt("Informe seu nome: ");
     console.log(`Olá ${nome}!`);
 }
+
 //exercio2();
 
 function exercio3(){
@@ -22,4 +23,23 @@ function exercio3(){
 
 }
 //exercio3();
+
+function exercio4(){
+    let idade = parseInt(prompt("Informe a Idade: "));
+
+    if(idade <= 0 ){
+        console.log("Informe um valor maior que zero!");
+        return exercio4();
+    }else if(idade < 18){
+        console.log("A pessoa é menor de idade!");
+    }else if(idade >= 18 && idade <= 122){
+        console.log("A pessoa é maior de idade!");
+    }else if(idade > 122){
+        console.log("A pessoa é a mais velha de todos os tempos!");
+    }else{
+        console.log("Valor Inválido! Informe apenas números!");
+        return exercio4();
+    }
+}
+exercio4();
 
